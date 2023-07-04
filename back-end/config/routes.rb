1f020b375ebf31ad823823686e120843624e1f0b
MullahStanley <mullah.kimani@student.moringaschool.com>
2023-07-04 12:15:23 +0300

    update routes

--- back-end/config/routes.rb
+++ back-end/config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  resources :patients
+  resources :patients, only[:index, :create, :destroy]
   resources :nurses
   resources :doctors
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  post '/patients', to: 'patients#create'
+  post '/login', to: 'patients#login'
+  get '/appointments', to: 'patients#appointments'
+  post '/appointments', to: 'patients#book_appointment'
+  delete '/appointments/:id', to: 'patients#delete_appointment'
   # Defines the root path route ("/")
   # root "articles#index"
 end
