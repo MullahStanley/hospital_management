c8e6dc483b11b2029fdd88571b1074fb9972154c
MullahStanley <mullah.kimani@student.moringaschool.com>
2023-07-04 12:13:15 +0300

    update controller

--- back-end/app/controllers/patients_controller.rb
+++ back-end/app/controllers/patients_controller.rb
@@ -1,2 +1,49 @@
 class PatientsController < ApplicationController
+    
+    def create
+        patient = Patient.new(patient_params)
+        if patient.save
+            token = patient.generate_token
+            render json: { patient: patient, token: token },status::created
+        else
+            render json: { error:'Invalid email or password' }, status: :unauthorised
+        end
+    end
+
+    def appointments
+        patient = current_patient
+        appointments = patient.appointments
+        render json: appointments
+      end
+    
+      def book_appointment
+        patient = current_patient
+        doctor = Doctor.find(params[:doctor_id])
+        appointment = patient.appointments.build(doctor: doctor, scheduled_at: params[:scheduled_at])
+        if appointment.save
+          render json: appointment, status: :created
+        else
+          render json: { errors: appointment.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+    
+      def delete_appointment
+        patient = current_patient
+        appointment = patient.appointments.find(params[:id])
+        appointment.destroy
+        head :no_content
+      end
+    
+      private
+    
+      def patient_params
+        params.require(:patient).permit(:name, :email, :password, :password_confirmation)
+      end
+    
+      def current_patient
+        token = request.headers['Authorization']
+        UserToken.find_by(token: token)&.user
+      end
+    end
+
 end
